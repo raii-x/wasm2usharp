70c65836e6c3cc426a077726408b689f55fe745c
raii <raii.program@gmail.com>
2023-10-12 00:46:32 +0900

    main関数をlib.rs内に移動

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,48 @@
 pub mod converter;
 pub mod util;
+
+use std::{env, process::ExitCode};
+
+use wasmparser::validate;
+
+use converter::Converter;
+
+pub fn lib_main() -> ExitCode {
+    let mut file_path = None;
+    let mut test = false;
+
+    let args = env::args().collect::<Vec<_>>();
+    for arg in &args[1..] {
+        match arg.as_str() {
+            "--test" => test = true,
+            _ => match file_path {
+                Some(_) => {
+                    // 入力ファイルが2個以上指定されている場合はエラー
+                    show_usage(&args[0]);
+                    return ExitCode::FAILURE;
+                }
+                None => file_path = Some(arg),
+            },
+        }
+    }
+
+    let file_path = match file_path {
+        Some(x) => x,
+        None => {
+            // 入力ファイルが指定されていない場合はエラー
+            show_usage(&args[0]);
+            return ExitCode::FAILURE;
+        }
+    };
+
+    let buf: Vec<u8> = std::fs::read(file_path).unwrap();
+    validate(&buf).unwrap();
+    let mut converter = Converter::new(&buf, "Wasm2USharp", test);
+    converter.convert(&mut std::io::stdout()).unwrap();
+
+    ExitCode::SUCCESS
+}
+
+fn show_usage(program: &str) {
+    println!("Usage: {} [--test] file", program);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,5 @@
-use std::{env, process::ExitCode};
+use wasm2usharp::lib_main;
 
-use wasmparser::validate;
-
-use wasm2usharp::converter::Converter;
-
-fn main() -> ExitCode {
-    let mut file_path = None;
-    let mut test = false;
-
-    let args = env::args().collect::<Vec<_>>();
-    for arg in &args[1..] {
-        match arg.as_str() {
-            "--test" => test = true,
-            _ => match file_path {
-                Some(_) => {
-                    // 入力ファイルが2個以上指定されている場合はエラー
-                    show_usage(&args[0]);
-                    return ExitCode::FAILURE;
-                }
-                None => file_path = Some(arg),
-            },
-        }
-    }
-
-    let file_path = match file_path {
-        Some(x) => x,
-        None => {
-            // 入力ファイルが指定されていない場合はエラー
-            show_usage(&args[0]);
-            return ExitCode::FAILURE;
-        }
-    };
-
-    let buf: Vec<u8> = std::fs::read(file_path).unwrap();
-    validate(&buf).unwrap();
-    let mut converter = Converter::new(&buf, "Wasm2USharp", test);
-    converter.convert(&mut std::io::stdout()).unwrap();
-
-    ExitCode::SUCCESS
-}
-
-fn show_usage(program: &str) {
-    println!("Usage: {} [--test] file", program);
+fn main() -> std::process::ExitCode {
+    lib_main()
 }
