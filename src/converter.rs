a3ff74664fe989f54594869073328d92302fab5c
raii <raii.program@gmail.com>
2023-10-21 17:55:54 +0900

    テストで識別子が予約語の場合にエラーが出ていたのを修正

--- src/converter.rs
+++ src/converter.rs
@@ -1,6 +1,6 @@
 mod code;
 
-use std::{fmt, io::Write};
+use std::{collections::HashSet, fmt, io::Write};
 
 use anyhow::Result;
 use once_cell::sync::Lazy;
@@ -683,7 +683,89 @@ fn get_cs_ty(ty: ValType) -> &'static str {
 }
 
 pub fn convert_to_ident(name: &str) -> String {
-    let prefix = if name.chars().next().unwrap().is_ascii_digit() {
+    static KEYWORDS: Lazy<HashSet<&str>> = Lazy::new(|| {
+        HashSet::from([
+            "abstract",
+            "as",
+            "base",
+            "bool",
+            "break",
+            "byte",
+            "case",
+            "catch",
+            "char",
+            "checked",
+            "class",
+            "const",
+            "continue",
+            "decimal",
+            "default",
+            "delegate",
+            "do",
+            "double",
+            "else",
+            "enum",
+            "event",
+            "explicit",
+            "extern",
+            "false",
+            "finally",
+            "fixed",
+            "float",
+            "for",
+            "foreach",
+            "goto",
+            "if",
+            "implicit",
+            "in",
+            "int",
+            "interface",
+            "internal",
+            "is",
+            "lock",
+            "long",
+            "namespace",
+            "new",
+            "null",
+            "object",
+            "operator",
+            "out",
+            "override",
+            "params",
+            "private",
+            "protected",
+            "public",
+            "readonly",
+            "ref",
+            "return",
+            "sbyte",
+            "sealed",
+            "short",
+            "sizeof",
+            "stackalloc",
+            "static",
+            "string",
+            "struct",
+            "switch",
+            "this",
+            "throw",
+            "true",
+            "try",
+            "typeof",
+            "uint",
+            "ulong",
+            "unchecked",
+            "unsafe",
+            "ushort",
+            "using",
+            "virtual",
+            "void",
+            "volatile",
+            "while",
+        ])
+    });
+
+    let prefix = if name.chars().next().unwrap().is_ascii_digit() || KEYWORDS.contains(name) {
         "_"
     } else {
         ""
